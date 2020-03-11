42f8028d0a411333fe1176e7a8051ad78e054289
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-03-10 18:01:55 -0600

    Practica 6 created

---
+++ practica6/js/practica6.js
@@ -0,0 +1,41 @@
+var coordenadas = {
+    lat: 0,
+    lng: 0
+}
+
+var propiedades = {
+    center: coordenadas,
+    zoom: 20
+}
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById("map"), propiedades);
+
+    var icono = {
+        url: "https://d1s6fstvea5cci.cloudfront.net/content/themes/vtnz/resources/assets/images/pulse_dot.gif"
+    }
+
+    var marker = new google.maps.Marker({
+        position: { lat: 0, lng: 0},
+        icon: icono,
+        scaledSize: new google.maps.Size(50, 50),
+        map: map
+    });
+
+    if(navigator.geolocation)
+    {
+        moverPosicion(marker);
+    }
+}
+
+function moverPosicion(marker){
+    navigator.geolocation.getCurrentPosition(posicion => {
+        var pos = {
+            lat: posicion.coords.latitude,
+            lng: posicion.coords.longitude
+        }
+
+        marker.setPosition(new google.maps.LatLng(pos.lat, pos.lng));
+        map.panTo(new google.maps.LatLng(pos.lat, pos.lng));
+    });
+}
