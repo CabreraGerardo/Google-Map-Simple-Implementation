c4b5933aa31ec033f3a443e6824f3568c7483f66
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-03-24 17:59:09 -0600

    Added Practica 4 functionality

--- Fetch/Practica4/js/practica4.js
+++ Fetch/Practica4/js/practica4.js
@@ -9,37 +9,29 @@ var propiedades ={
 }
 
 function obtenerMarcadores(){
-    const markers = [
-        {
-            name: "Mexico",
-            longitude: "-99.1276",
-            latitude: "19.427"
-        },
-        {
-            name: "Brazil",
-            longitude: "-47.9292",
-            latitude: "-15.7801"
-        },
-        {
-            name: "Spain",
-            longitude: "-3.70327",
-            latitude: "40.4167"
-        }
-    ];
+    
+    fetch('https://corona.lmao.ninja/countries')
+    .then((response) => {
+        response.json().then((datos) => {
+            datos.forEach((marcador) => {
+                console.log(marcador);
 
-    return markers;
+                let marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(marcador.countryInfo.lat, marcador.countryInfo.long),
+                    title: marcador.country
+                });
+
+            });
+        });
+    })
+    .catch((error) => {
+        console.log('Hubo un problema con la petición Fetch: ' + error.message);
+    });
 };
 
 function initMap(){
     const map = new google.maps.Map(document.getElementById("map"), propiedades);
 
     const markers = obtenerMarcadores();
-
-    for(marcador in markers){
-        let marker = new google.maps.Marker({
-            map: map,
-            position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
-            title: marcador.name
-        });
-    }
 };
