d2d9f47876aa623ac79036727e650030806b30fc
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-03-24 18:21:54 -0600

    addeed info window and custom icon

--- Fetch/Practica4/js/practica4.js
+++ Fetch/Practica4/js/practica4.js
@@ -10,16 +10,34 @@ var propiedades ={
 
 function obtenerMarcadores(map){
     
+    var icono = {
+        url: "https://cdn.pixabay.com/photo/2020/02/26/08/41/corona-4881111_1280.png",
+        scaledSize: new google.maps.Size(25, 25),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+    }
+
     fetch('https://corona.lmao.ninja/countries')
     .then((response) => {
         response.json().then((datos) => {
             datos.forEach((marcador) => {
                 console.log(marcador);
+                
+                var infoContent = "<h3>" + marcador.country + "</h3>";
+
+                var info = new google.maps.InfoWindow({
+                    content: infoContent
+                });
 
                 let marker = new google.maps.Marker({
                     map: map,
                     position: new google.maps.LatLng(marcador.countryInfo.lat, marcador.countryInfo.long),
-                    title: marcador.country
+                    title: marcador.country + ': ' + marcador.cases,
+                    icon: icono
+                });
+
+                marker.addListener('click', () => {
+                    info.open(map, marker);
                 });
 
             });
