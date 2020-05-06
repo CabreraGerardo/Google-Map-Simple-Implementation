a67242221e60b4b7bb945b480c0077b97c45ace6
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-05-05 20:38:51 -0500

    Added Practica 9 to index and dropdown.

---
+++ index.js
@@ -0,0 +1,256 @@
+document.getElementById('mainMaps').innerHTML += `
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica1/Marcador.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color:tomato; background-color: rgb(212, 241, 167);">
+                    <i class="fas fa-map-marker-alt m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 1</p>
+                    <p class="link-body">Implementación básica de un <b>Mapa de Google</b> y un <b>Marcador</b>.</p>    
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica2/navigator.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color:rgb(26, 99, 145); background-color: rgb(225, 206, 255);">
+                    <i class="fas fa-info-circle m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 2</p>
+                    <p class="link-body">Uso del objeto <b>Navigator</b> para obtener información del navegador.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica3/geolocation.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color:rgb(226, 99, 48); background-color: rgb(243, 229, 189);">
+                    <i class="fas fa-search-location m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 3</p>
+                    <p class="link-body">Uso de la geolocalización del objeto <b>Navigator</b> para obtener las coordenadas del dispositivo.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica4/static_map.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color:rgb(58, 57, 100); background-color: rgb(174, 218, 243);">
+                    <i class="fas fa-images m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 4</p>
+                    <p class="link-body">Uso de la api de <b>Static Map</b> y la geolocalización del objeto <b>Navigator</b> para la creración de una imágen estática de un mapa.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica5/infowindow.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(109, 119, 73); background-color:rgb(233, 248, 164);">
+                    <i class="far fa-window-maximize m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 5</p>
+                    <p class="link-body">Implementación de la herramienta <b>Info Window</b>.</p>
+                </div>
+            </div>
+        </a>
+    </li> 
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica6/current_location.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(141, 114, 85); background-color:rgb(198, 250, 156);">
+                    <i class="fas fa-search-location m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 6</p>
+                    <p class="link-body">Implementación de localización en tiempo real.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica7/practica7.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(194, 110, 110); background-color:rgb(162, 247, 228);">
+                    <i class="fas fa-object-group m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 7</p>
+                    <p class="link-body">Implementación de <b>MarkerCluster</b> para la agrupación de marcadores en un mapa.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica8/practica8.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(70, 51, 51); background-color:rgb(204, 203, 185);">
+                    <i class="fas fa-language m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 8</p>
+                    <p class="link-body">Uso de diferentes idiomas en un mapa.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Maps/Practica9/mapStyle.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: #8A888A; background-color:#681017;">
+                    <i class="fas fa-fill-drip m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 8</p>
+                    <p class="link-body">Uso de diferentes idiomas en un mapa.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+`;
+
+document.getElementById('mainFirebase').innerHTML += `
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Firebase/Practica1/alta_baja.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(255, 217, 0); background-color:rgb(250, 119, 102);">
+                    <i class="fas fa-fire m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 1</p>
+                    <p class="link-body">Implementación básica de <b>Firebase</b>. Registro y Borrado.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Firebase/Practica2/editar.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(152, 227, 230); background-color:rgb(66, 66, 66);">
+                    <i class="fas fa-file-code m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 2</p>
+                    <p class="link-body">Implementación de edición en <strong>Firebase</strong>. Segmentación de código en diferentes archivos.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <div class="row m-0">
+            <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(128, 211, 128); background-color:rgb(116, 116, 116);">
+                <i class="fas fa-user-secret m-3"></i>
+            </div>
+            <div class="col-12 col-md-10 pt-2">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h3 class="p-2 mx-5" style="font-weight: bold; color: rgb(1, 1, 102); border: 1px solid darkblue; border-radius: 15px;">Página Espía</h3>
+                    </div>
+                    <div class="col-12 col-md-6 m-0 text-center" style="margin-right: 0px !important;">
+                        <a class="link-title" href="Firebase/PosicionEspia/current_location.html">
+                            <p class="link-title">Envío de Coordenadas</p>
+                            <p class="link-body">Uso del objeto <strong>Navigator</strong> para obtener la ubicación actual. Uso de <strong>Firebase</strong> para almacenar las coordenadas.</p>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 m-0 text-center">
+                        <a class="link-title" href="Firebase/PosicionEspia/current_location_spy.html">
+                            <p class="link-title">Monitoreo de Coordenadas</p>
+                            <p class="link-body">Uso de <strong>Firebase</strong> para consultar coordenadas y mostrarlas en un <strong>Google Map</strong>.</p>   
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Firebase/FireAuth/index.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(138, 207, 159); background-color:rgb(98, 112, 138);">
+                    <i class="fas fa-address-card m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">OAuth</p>
+                    <p class="link-body">Implementación de <strong>Firebase OAuth</strong>.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+`;
+
+document.getElementById('mainFetch').innerHTML += `
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Fetch/Practica1/practica1.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(151, 151, 151); background-color:rgb(43, 42, 42);">
+                    <i class="fas fa-file m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 1</p>
+                    <p class="link-body">Uso del método <b>Fetch</b> para consultar un archivo <b>JSON</b> local.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Fetch/Practica2/practica2.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(226, 237, 243); background-color:rgb(117, 213, 245);">
+                    <i class="fas fa-server m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 2</p>
+                    <p class="link-body">Uso del método <b>Fetch</b> para consultar un archivo <b>JSON</b> en línea.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Fetch/Practica3/practica3.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(211, 37, 37); background-color:rgb(161, 231, 159);">
+                    <i class="fas fa-map-marker m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 3</p>
+                    <p class="link-body">Uso de <b>Google Maps</b> para mostrar diferentes marcadors usando un arreglo.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+
+    <li class="list-group-item rounded p-0 my-2 my-md-0">
+        <a class="link-title" href="Fetch/Practica4/practica4.html">
+            <div class="row m-0">
+                <div class="col-12 col-md-2 d-flex justify-content-center align-items-center rounded" style="font-size: 48px; color: rgb(207, 45, 113); background-color:rgb(201, 189, 207);">
+                    <i class="fas fa-virus m-3"></i>
+                </div>
+                <div class="col-12 col-md-10 pt-2">
+                    <p class="link-title">Práctica 4</p>
+                    <p class="link-body">Uso del método <b>Fetch</b> para consultar una <b>API</b> con las estadísticas del coronavirus en diferentes paises.</p>
+                </div>
+            </div>
+        </a>
+    </li>
+`;
--- public/js/public.js
+++ public/js/public.js
@@ -10,3 +10,30 @@ $('#btnDropdownPracticas').click(function() {
         document.getElementById('dropdownPracticas').style.right = '0px';
     }
 });
+
+document.getElementById('dropdownMaps').innerHTML += `
+    <a class="dropdown-item" href="../../Maps/Practica1/Marcador.html">Práctica 1</a>
+    <a class="dropdown-item" href="../../Maps/Practica2/navigator.html">Práctica 2</a>
+    <a class="dropdown-item" href="../../Maps/Practica3/geolocation.html">Práctica 3</a>
+    <a class="dropdown-item" href="../../Maps/Practica4/static_map.html">Práctica 4</a>
+    <a class="dropdown-item" href="../../Maps/Practica5/infowindow.html">Práctica 5</a>
+    <a class="dropdown-item" href="../../Maps/Practica6/current_location.html">Práctica 6</a>
+    <a class="dropdown-item" href="../../Maps/Practica7/practica7.html">Práctica 7</a>
+    <a class="dropdown-item" href="../../Maps/Practica8/practica8.html">Práctica 8</a>
+    <a class="dropdown-item" href="../../Maps/Practica9/mapStyle.html">Práctica 9</a>
+`;
+
+document.getElementById('dropdownFirebase').innerHTML += `
+    <a class="dropdown-item" href="../../Firebase/Practica1/alta_baja.html">Práctica 1</a>
+    <a class="dropdown-item" href="../../Firebase/Practica2/editar.html">Práctica 2</a>
+    <a class="dropdown-item" href="../../Firebase/PosicionEspia/current_location.html">Espía (Envío de Coordenada)</a>
+    <a class="dropdown-item" href="../../Firebase/PosicionEspia/current_location_spy.html">Espía (Consulta de Coordenada)</a>
+    <a class="dropdown-item" href="../../Firebase/FireAuth/index.html">Auth</a>
+`;
+
+document.getElementById('dropdownFetch').innerHTML += `    
+    <a class="dropdown-item" href="../../Fetch/Practica1/practica1.html">Práctica 1</a>
+    <a class="dropdown-item" href="../../Fetch/Practica2/practica2.html">Práctica 2</a>
+    <a class="dropdown-item" href="../../Fetch/Practica3/practica3.html">Práctica 3</a>
+    <a class="dropdown-item" href="../../Fetch/Practica4/practica4.html">Práctica 4</a>
+`;
