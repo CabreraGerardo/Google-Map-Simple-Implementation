4a6b70f105ce41987508a3acc771c2b010be03fb
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-05-12 20:31:10 -0500

    validation

--- Firebase/ShareLocation/js/sendData.js
+++ Firebase/ShareLocation/js/sendData.js
@@ -1,6 +1,6 @@
 function updatePosition(lat, lng){
     console.log(localStorage.user);
-    if(localStorage.user != '' || localStorage.user != null || localStorage.user != undefined)
+    if(localStorage.user)
     {
         db.collection("usuariosUbicacion").doc(localStorage.user).update({
             location: new firebase.firestore.GeoPoint(lat, lng)
